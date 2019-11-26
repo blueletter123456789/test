eb4dc2b49e45e66f97d6805591c7675f466c75b8
Shuhei Abe <blueletter123456789@gmail.com>
2019-11-26 22:55:39 +0900

    問い合わせフォームのバリデーション作成（tell）

--- JS/main.js
+++ JS/main.js
@@ -12,32 +12,68 @@ $(function(){
 
 
 /* SLIDER ANIMATION */
-$('.slider').slick({
-  centerMode: true,
-  prevArrow: '<span class="arrow prev-arrow"><i class="material-icons">arrow_left</i></span>',
-  nextArrow: '<span class="arrow next-arrow"><i class="material-icons">arrow_right</i></span>',
-  centerPadding: '0px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+  $('.slider').slick({
+    centerMode: true,
+    prevArrow: '<span class="arrow prev-arrow"><i class="material-icons">arrow_left</i></span>',
+    nextArrow: '<span class="arrow next-arrow"><i class="material-icons">arrow_right</i></span>',
+    centerPadding: '0px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
+    ]
+  });
+
+// contact validation
+  $('input, textarea').on('blur', function(){
+    var name = $(this).attr('name');
+    var val = $(this).val();
+    console.log(name + ':' + val);
+
+    if (name == 'tel'){
+      chkTel(val);
     }
-  ]
-});
+    
+  });
+
+  function chkTel(val){
+    if (val.match(/^\d{10,11}$/)){
+      console.log('OK');
+    }else{
+      console.log('NG');
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
