ff276bf6c7e611061527c7246ef06cfe106fb20a
Shuhei Abe <blueletter123456789@gmail.com>
2019-12-02 00:54:52 +0900

    レスポンシブ対応（スマホ、タブレット）

--- JS/main.js
+++ JS/main.js
@@ -41,34 +41,109 @@ $(function(){
   });
 
 // contact validation
+  // $('input, textarea').on('blur', function(){
+  //   var name = $(this).attr('name');
+  //   var val = $(this).val();
+  //   console.log(name + ':' + val);
+
+  //   if (name == 'tel'){
+  //     chkTel(val);
+  //   }
+    
+  // });
+
+  // function chkTel(val){
+  //   if (val.match(/^\d{10,11}$/)){
+  //     console.log('OK');
+  //   }else{
+  //     console.log('NG');
+  //   }
+  // }
+
+  var errorMessage = {
+    'name' : '', 
+    'mail' : '', 
+    'tel' : '', 
+    'subject' : '', 
+    'content' : '', 
+  };
+
   $('input, textarea').on('blur', function(){
     var name = $(this).attr('name');
     var val = $(this).val();
-    console.log(name + ':' + val);
 
-    if (name == 'tel'){
-      chkTel(val);
+    if(!emptyCheck(name, val)){
+      ouputMessage(name);
+      return;
+    };
+
+    if (name == 'tel') {
+      telCheck(val);
+    }else if(name == 'mail'){
+      mailCheck(val);
+    }else{
+      symbolCheck(name, val);
     }
-    
+
+    console.log(errorMessage);
+
+    ouputMessage(name);
+
+
+
   });
 
-  function chkTel(val){
-    if (val.match(/^\d{10,11}$/)){
-      console.log('OK');
+  function emptyCheck(name, val){
+    // if (val=='' ) {
+    //   errorMessage[name] = '必須項目です';
+    // }else{
+    //   errorMessage[name] = ''; 
+    // }
+
+    errorMessage[name] = (val == '') ? '必須項目です' : '';
+    return (val=='') ? false : true;
+
+  };
+
+  function telCheck(val){
+    // console.log(val.match(/^\d{10,11}$/));
+    // if (val.match(/^\d{10,11}$/)){
+    //   errorMessage['tel'] = '';
+    // }else{
+    //   errorMessage['tel'] = '形式が正しくありません';
+    // }
+
+  // 三項演算子
+  errorMessage['tel'] = (val.match(/^\d{10,11}$/)) ? '' : '電話の形式が正しくありません';
+
+  };
+
+  function symbolCheck(name, val){
+  errorMessage[name] = (val.match(/[!"#$%&'()\*\+\-\.,\/:;<=>?@\[\\\]^_`{|}~]/g)) ? '入力値が正しくありません' : '';
+  };
+
+  function mailCheck(val){
+    if (!val.match(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9]{1}[A-Za-z0-9_.-]*\.[A-Za-z0-9]{1,}$/)){
+      errorMessage['mail'] = 'メールの記載が正しくありません'
     }else{
-      console.log('NG');
+      errorMessage['mail'] = (val.match(/[_.-]{2,}/g)) ? 'メールの記載が正しくないよ' : '';
     }
-  }
+  };
 
 
-
-
-
-
-
-
-
-
+  function ouputMessage(name){
+    var cnt = 0;
+    for(key in errorMessage){
+      $('input[name="' + name + '"] ~ span.error-message').text(errorMessage[name]);
+      $('textarea[name="' + name + '"] ~ span.error-message').text(errorMessage[name]);
+      if(errorMessage[key] != '') cnt++;
+    }
+      if(cnt > 0){
+        $('input[name="submit"]').attr('disabled', true);
+      }else{
+        $('input[name="submit"]').attr('disabled', false);
+      }
+  };
 
 
 
