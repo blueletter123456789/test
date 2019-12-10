7977210d8dacfdca16815fff23a3254a87baa006
YoheyKondo <y_kondo@excitingworks.jp>
2019-12-10 17:58:22 +0900

    JQueryScrollifyイベント編集、表示されるsectionに対するクラス付与処理参考用

--- test/jquery_scrollify/setScrollify.js
+++ test/jquery_scrollify/setScrollify.js
@@ -1,12 +1,18 @@
 $(function(){
 	var current;
-
+	var sections_array = [];
+	var sections = $('div.wrapper > section');
+	// https://webdesignday.jp/inspiration/technique/jquery-js/3812/
+	// プロパティ記述参考サイト
 	$.scrollify({
 		section:"section",
 		setHeights:false,
 		scrollbars:false,
 			before:function(i,section){
 				current = i;
+			},
+			after:function(num,section){
+				addActive(num);
 			},
 		});
 		
@@ -16,4 +22,26 @@ $(function(){
 			$(window).scrollTop(currentScrl);
 		}
 	});
+	/* SECTION ANIMATION */
+	// セクションごとの要素を連想配列に格納
+	$(window).on('load resize', function(){
+		sections.each(function(){
+			var id = $(this).attr('id');
+			var top = $(this).offset().top;
+			var bottom = top + $(this).outerHeight();
+			var sec = {'id':id,'top':top,'bottom':bottom};
+			sections_array.push(sec);
+		});
+		
+	});
+	// 表示されているセクションにクラスを付与する
+	function addActive(num){
+		sections.each(function(){
+			$(this).removeClass('active');
+		});
+		var id = sections_array[num].id;
+		if(id!=undefined){
+			$('#'+id).addClass('active');
+		}
+	}
 })
