a6de64044b69887375c62ae43bb3f25342043b2d
YoheyKondo <y_kondo@excitingworks.jp>
2019-12-10 18:56:57 +0900

    連想配列処理変更、プロパティ内の処理を活用修正

--- test/jquery_scrollify/setScrollify.js
+++ test/jquery_scrollify/setScrollify.js
@@ -8,40 +8,26 @@ $(function(){
 		section:"section",
 		setHeights:false,
 		scrollbars:false,
+		scrollSpeed:1000,
 			before:function(i,section){
 				current = i;
 			},
 			after:function(num,section){
-				addActive(num);
+				sections.each(function(){
+					$(this).removeClass('active');
+				})
+				$(section[num]).addClass('active');
 			},
+			afterResize:function(){
+				if(current){
+					var currentScrl = $('section').eq(current).offset().top;
+					$(window).scrollTop(currentScrl);
+				}
+				$('nav').css('display','none');
+				setTimeout(function(){
+					$('nav').css('display','block');
+				}, 1);
+			}
 		});
 		
-	$(window).on('resize',function(){
-		if(current){
-			var currentScrl = $('section').eq(current).offset().top;
-			$(window).scrollTop(currentScrl);
-		}
-	});
-	/* SECTION ANIMATION */
-	// セクションごとの要素を連想配列に格納
-	$(window).on('load resize', function(){
-		sections.each(function(){
-			var id = $(this).attr('id');
-			var top = $(this).offset().top;
-			var bottom = top + $(this).outerHeight();
-			var sec = {'id':id,'top':top,'bottom':bottom};
-			sections_array.push(sec);
-		});
-		
-	});
-	// 表示されているセクションにクラスを付与する
-	function addActive(num){
-		sections.each(function(){
-			$(this).removeClass('active');
-		});
-		var id = sections_array[num].id;
-		if(id!=undefined){
-			$('#'+id).addClass('active');
-		}
-	}
 })
