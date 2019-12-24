bdc60d54d4817091dfcc01798f87ba1631796296
Shuhei Abe <blueletter123456789@gmail.com>
2019-12-24 22:28:34 +0900

    【作成】タイマーアプリ_機能

--- app/timer/JS/main.js
+++ app/timer/JS/main.js
@@ -1,4 +1,4 @@
-var timer = new Vue({
+const timer = new Vue({
 	el:'#timer', 
 
 	// 変数定義エリア
@@ -6,37 +6,59 @@ var timer = new Vue({
 		hour:0, 
 		minute:0, 
 		second:0,
-		status:true,
-		disp:true,
-		start_flg:0, 
+		btn_status:true,
+		disp:true, 
+		obj:null,
 	},
 
 	// アプリ内関数
 	methods:{
 		// カウントダウン関数（スタート用関数）
-		countDown:function(){
+		start(){
 
-			this.status = false;
+			// console.log('chk');
+
+			this.btn_status = false;
 			this.disp = false;
-			this.start_flg = 1;
 
 			let self = this;
 			this.obj = setInterval(function(){self.countDown()},1000);
 		},
 		// リセット関数
-		reset:function(){
+		reset(){
 			this.hour = 0;
 			this.minute = 0;
 			this.second = 0;
-			this.start_flg = 0;
+			this.disp = true;
+			this.btn_status = true;
 		},
-		// ストップ関数
-		stop:function(){
-			this.status = true;
-		},
-		// カウントセット関数
-		countSet:function(){
 
+		// ストップ関数
+		stop(){
+			this.btn_status = true;
+			clearInterval(this.obj);
 		},
-	}
+
+		// カウントダウン実施関数
+		countDown(){
+			if (this.second==0) {
+				if(this.hour==0 && this.minute==0) {
+					alert('完了！');
+					this.stop();
+					this.reset();
+					return;
+				}else if(this.minute>0){
+					this.minute--;
+					this.second=59;
+				}else if(this.hour>0){
+					this.hour--;
+					this.minute=59;
+					this.second=59;
+				}	
+			}else{
+				this.second--;
+			}
+		},
+
+	},
 });
