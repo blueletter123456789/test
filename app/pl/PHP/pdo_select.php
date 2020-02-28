8c28a47b74796a6277f0e40c8dcc2655a85b90ea
Shuhei Abe <blueletter123456789@gmail.com>
2020-02-28 20:35:48 +0900

    【修正】SQLフラグ判定

--- app/pl/PHP/pdo_select.php
+++ app/pl/PHP/pdo_select.php
@@ -20,7 +20,7 @@
 
 	// $stmt -> execute();
 
-	$stmt = $pdo -> query('SELECT i.input_code as "input_code", i.input_date as "input_date", u.use_name as "use_name", d.account_name as account_name, i.amount as "amount" FROM TBL_INPUT i INNER JOIN TBL_USE u on i.use_code = u.use_code INNER JOIN TBL_ACCOUNT_DETAIL d on i.account_code = d.account_code');
+	$stmt = $pdo -> query('SELECT i.input_code as "input_code", i.input_date as "input_date", u.use_name as "use_name", d.account_name as account_name, i.amount as "amount" FROM TBL_INPUT i INNER JOIN TBL_USE u on i.use_code = u.use_code INNER JOIN TBL_ACCOUNT_DETAIL d on i.account_code = d.account_code WHERE u.use_flg = "0" AND d.account_flg = "0"');
 
 	$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
