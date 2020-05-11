b81040e740a11dd59872aaad14df881be92dfe99
blueletter123456789 <blueletter123456789@gmail.com>
2020-05-12 00:17:02 +0900

    【修正】ポートフォリオサイト全体構成

--- portfolio_v2/js/main.js
+++ portfolio_v2/js/main.js
@@ -1,14 +1,22 @@
-$(function() {
+$(function () {
+  var windowHeight = $(window).height();
+  var windowWidth = $(window).width();
+  console.log(windowHeight);
+  console.log(windowWidth);
 
-	$('#about').click(function(){
-	var speed = 1000;　//スムーズスクロールスピード
-	var href= $(this).attr("href");
-	var target = $(href == "#" || href == "" ? 'html' : href);
-	var position = target.offset().top;
-	$("html, body").animate({scrollTop:position}, speed, "swing");
-	return false;
- 	});
+  $("#about").click(function () {
+    var speed = 1000; //スムーズスクロールスピード
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 
-	var controller = new ScrollMagic.Controller();
+  //   フォームクリックイベント
+  $('#form-inside input[type="text"]').on("click", function () {
+    $("#form-inside span");
+  });
 
+  var controller = new ScrollMagic.Controller();
 });
