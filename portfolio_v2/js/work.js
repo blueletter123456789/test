3b54a59eb1d5f39fe74a043b116a5f3534070237
Shuhei Abe <blueletter123456789@gmail.com>
2020-05-21 21:12:36 +0900

    【修正】portfoliov2_各セクション

--- portfolio_v2/js/work.js
+++ portfolio_v2/js/work.js
@@ -2,18 +2,57 @@ $(function () {
 
 	var controller = new ScrollMagic.Controller();
 
-	var panels = $(".panel");
-	console.log(panels);
+	var windowH = $(window).height();
 
-	for (var i = 1; i < panels.length; i++) {
+		// define movement of panels
+/*		var wipeAnimation = new TimelineMax()
+			.fromTo(".panel.p2", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+			.fromTo(".panel.p3",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+			.fromTo(".panel.p4", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone}) // in from top
+			.fromTo(".panel.p5", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone}); // in from top
+
+		// create scene to pin and link animation
 		new ScrollMagic.Scene({
-				triggerElement: panels[i],
+				triggerElement: "#top-slider",
 				triggerHook: "onLeave",
-				duration: "400%"
+				duration: "500%"
+			})
+			.setPin("#top-slider")
+			.setTween(wipeAnimation)
+			.addTo(controller);*/
+
+	var scene1 = new ScrollMagic.Scene({
+			triggerElement: "#panel1", 
+			triggerHook: "onLeave"
 		})
-		.setPin(panels[i], {pushFollowers: false})
-		.addIndicators()
+		.setPin(".p1")
+		.addIndicators({name: "scroll-target"})
 		.addTo(controller);
-	}
+
+	var scene2 = new ScrollMagic.Scene({
+			triggerElement: "#panel2", 
+			triggerHook: "onLeave"
+		})
+		.setPin(".p2")
+		.addIndicators({name: "scroll-target"})
+		.addTo(controller);
+
+	var scene3 = new ScrollMagic.Scene({
+			triggerElement: "#panel3", 
+			triggerHook: "onLeave", 
+		})
+		.setPin(".p3")
+		.addIndicators({name: "scroll-target"})
+		.addTo(controller);
+
+	var scene6 = new ScrollMagic.Scene({
+			triggerElement: "#all-works", 
+			triggerHook: "onEnter"
+		})
+		.setClassToggle("#all-works", "active") 
+		.addIndicators({name: "scroll-target"})
+		.addTo(controller);
+
+
 
 });
